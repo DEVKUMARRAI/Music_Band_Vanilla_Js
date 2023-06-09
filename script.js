b11e87a76919cfb4afad9db2ee66c380b4fc943c
Dev <devrai197551@gmail.com>
2023-06-09 20:41:33 +0530

    Js done

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-inp="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-inp="${e.keyCode}"]`)
     if (audio == null) {
         return;
     }
     audio.currentTime = 0;
     audio.play();
-
+    key.classList.add('playing');
+    setTimeout(()=>{key.classList.remove('playing')},200)
+    
 })
